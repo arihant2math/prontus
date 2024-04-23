7bd6bd47e70dbcb62bb81950be11c01592234b38
Ashwin Naren <arihant2math@gmail.com>
2024-04-22 17:53:22 -0700

    working sidebar

--- src/secret.rs
+++
@@ -1,3 +0,0 @@
-pub static PRONTO_SESSION: &str = "eyJpdiI6ImNBK0Q5SDN1aU5TWGgxRzBPZFdUUXc9PSIsInZhbHVlIjoiNU9YNmIzRGdDVFZhVG13ZCtBSkRrN3h0OTc5a01HZHh2MlhMS1hhRThOcGZtdjhSVFdSY3Z1UlFvQ016WXcvOVMyN1ExUEZYMThPT1Y4YWJKYzVaWk1HUmFIUS9ISDJRMkZkeU9HZ0NIaDZRdUJhcy9aU0tpOFNJV2gydWlFSnUiLCJtYWMiOiJmMzhiYTUwNjFlZGIzODVhNzMxYzYxNWQxNDczZDJmZjM4ZGUzNDJjYjE3ODFiM2ZhNzc0YmVkNTNlYzYxYjc5IiwidGFnIjoiIn0=";
-pub static PRONTO_API_TOKEN: &str = "4puwrQyyFa4p10lZRrXgVwkFsY1eogUX3Qmk8EEa.1201495637";
-pub static PAACT_2245_5302428: &str = "stanfordohs.pronto.io|Ashwin|Naren|Stanford Online High School|stanfordohs|/files/orgs/2245/profilepic?pronto_time=1682388066";
