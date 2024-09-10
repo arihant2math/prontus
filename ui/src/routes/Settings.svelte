77f9e6afeed752fe80c6038c1a720a9154e03975
Ashwin Naren <arihant2math@gmail.com>
2024-09-10 09:33:00 -0700

    feat: icons on settings

--- ui/src/routes/Settings.svelte
+++ ui/src/routes/Settings.svelte
@@ -1,5 +1,4 @@
 <!--TODO: Add ability to log out and set theme-->
-<!--TODO: should this be a modal or a page?-->
 <script context="module">
     export function hideSettings() {
         let modal = document.getElementById("settings-modal");
@@ -42,7 +41,12 @@
                             <label for="settings-theme-automatic"
                                    class="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
                                 <div class="block">
-                                    <div class="w-full text-lg font-semibold">Automatic</div>
+                                    <div class="w-full text-lg font-semibold">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+                                        </svg>
+                                        <span class="ml-3">Automatic</span>
+                                    </div>
                                 </div>
                             </label>
                         </li>
@@ -50,15 +54,23 @@
                             <input type="radio" id="settings-theme-light" name="theme" value="light" class="hidden peer">
                             <label for="settings-theme-light" class="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
                                 <div class="block">
-                                    <div class="w-full text-lg font-semibold">Light</div>
+                                    <div class="w-full text-lg font-semibold flex flex-row">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mt-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                                        </svg>
+                                        <span class="ml-3">Light</span>
+                                    </div>
                                 </div>
                             </label>
                         </li>
                         <li>
                             <input type="radio" id="settings-theme-dark" name="theme" value="right" class="hidden peer">
                             <label for="settings-theme-dark" class="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
-                                <div class="block">
-                                    <div class="w-full text-lg font-semibold">Dark</div>
+                                <div class="w-full text-lg font-semibold flex flex-row">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mt-1">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                                    </svg>
+                                    <span class="ml-3">Dark</span>
                                 </div>
                             </label>
                         </li>
