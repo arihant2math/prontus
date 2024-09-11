7dc77fa0f50c4be30546e0c8b1b17af347762b5e
Ashwin Naren <arihant2math@gmail.com>
2024-09-11 10:48:42 -0700

    feat: made initials profile picture unselectable

--- ui/src/routes/ProfilePicture.svelte
+++ ui/src/routes/ProfilePicture.svelte
@@ -14,7 +14,7 @@
 {#if !flag}
     <img class="w-8 h-8 rounded-full" src="{user.profilepicurl}" alt="{user.fullname} image" on:error={setFlag}>
 {:else}
-    <div class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-        <span class="font-sm text-gray-600 dark:text-gray-300">{initials}</span>
+    <div class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-200 rounded-full dark:bg-gray-600">
+        <span class="font-sm text-gray-600 dark:text-gray-300 select-none">{initials}</span>
     </div>
 {/if}
