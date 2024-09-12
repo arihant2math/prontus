2f27554f255fc3b2908fc535b3e12edd0bf8e8f1
Ashwin Naren <arihant2math@gmail.com>
2024-09-12 11:16:16 -0700

    feat: made settings options editable

--- ui/src/routes/Settings.svelte
+++ ui/src/routes/Settings.svelte
@@ -18,8 +18,14 @@
     import {getSettings, setSettings} from "$lib/api.js";
 
     let settings = {
-        theme: "Auto",
-        notifications: true,
+        appearance: {
+            "theme": "Auto"
+        },
+        options: {
+            "richText": false,
+            "experiments": false,
+            "notifications": false
+        }
     };
 
     $: settings = settings;
@@ -107,24 +113,35 @@
                     </h4>
                     <ul class="grid w-full gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-xl">
                         <li>
-                            <input type="checkbox" id="notifications-option" value="" class="hidden peer" disabled>
-                            <OptionsLabel target="notifications-option">
-                                <svg slot="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mb-2 w-7 h-7 text-blue-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                            <input type="checkbox" id="rich-text-option" value="" class="hidden peer" bind:group={settings.options.richText} on:change={saveSettings}>
+                            <OptionsLabel target="rich-text-option">
+                                <svg slot="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mb-2 w-7 h-7 text-yellow-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                 </svg>
-
-                                <p slot="title">Experiments</p>
-                                <p slot="body">Be the first to try out new features that could be unstable.</p>
+                                <p slot="title">Rich Text</p>
+                                <p slot="body">Enabled rich text, which allows for <b>bold</b>, <i>italics</i>, links,
+                                    and more.</p>
                             </OptionsLabel>
                         </li>
                         <li>
-                            <input type="checkbox" id="notifications-option" value="" class="hidden peer" disabled>
+                            <input type="checkbox" id="notifications-option" value="" class="hidden peer" bind:group={settings.options.notifications} on:change={saveSettings}>
                             <OptionsLabel target="notifications-option">
                                 <svg slot="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mb-2 w-7 h-7 text-yellow-500">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                                 </svg>
                                 <p slot="title">Notifications</p>
                                 <p slot="body">Sends OS notifications when there are new messages.</p>
+                            </OptionsLabel>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="experiments-option" value="" class="hidden peer" bind:group={settings.options.experiments} on:change={saveSettings}>
+                            <OptionsLabel target="experiments-option">
+                                <svg slot="svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mb-2 w-7 h-7 text-blue-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                                </svg>
+
+                                <p slot="title">Experiments</p>
+                                <p slot="body">Be the first to try out new features that could be unstable.</p>
                             </OptionsLabel>
                         </li>
                     </ul>
