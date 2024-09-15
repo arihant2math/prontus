aad1a285c58fee7076dd568fd7f689e6f11a9c3e
Ashwin Naren <arihant2math@gmail.com>
2024-09-15 11:06:52 -0700

    chore: updated look of view thread button

--- ui/src/routes/Message.svelte
+++ ui/src/routes/Message.svelte
@@ -187,8 +187,8 @@
             </ul>
         </div>
         {#if lastThreadMessage && parentMessage !== undefined && !inThread}
-            <button on:click={() => {viewThread(parentMessage.id)}} type="button" class="max-w-fit text-gray-900 bg-white hover:bg-gray-100 font-medium
-            rounded-lg text-xs mx-2 px-2 py-2 me-2 mb-2 dark:bg-slate-800 dark:text-white dark:border-gray-600
+            <button on:click={() => {viewThread(parentMessage.id)}} type="button" class="max-w-fit text-blue-500 dark:text-blue-400 bg-white hover:bg-gray-100 font-medium
+            rounded-lg text-xs mx-2 px-2 py-2 me-2 mb-2 dark:bg-slate-900 dark:border-gray-600
             dark:hover:bg-gray-700 dark:hover:border-gray-600">View Thread</button>
         {/if}
     </div>
