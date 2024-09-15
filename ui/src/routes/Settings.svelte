d8a94952784130efdf12a32b9ac5e1def77dc7c6
Ashwin Naren <arihant2math@gmail.com>
2024-09-15 16:26:59 -0700

    feat: logout

--- ui/src/routes/Settings.svelte
+++ ui/src/routes/Settings.svelte
@@ -49,6 +49,11 @@
         }
     }
 
+    function logout() {
+        settings.auth.api_key = "";
+        saveSettings();
+    }
+
     loadTheme();
 
     loadSettings();
@@ -157,7 +162,7 @@
                     <h4 class="text-lg font-semibold text-gray-900 dark:text-white">
                         Account
                     </h4>
-                    <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700">
+                    <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700" on:click={logout}>
                         Logout
                     </button>
                 </form>
