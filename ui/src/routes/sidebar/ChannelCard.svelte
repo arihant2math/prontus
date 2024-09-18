a4fca6916c9d5689f47560715acb465cb1d3f83a
Ashwin Naren <arihant2math@gmail.com>
2024-09-17 17:06:59 -0700

    feat: sidebar context menu

--- ui/src/routes/sidebar/ChannelCard.svelte
+++ ui/src/routes/sidebar/ChannelCard.svelte
@@ -1,5 +1,6 @@
 <script>
     import ProfilePicture from "../ProfilePicture.svelte";
+    import {ContextMenu} from "bits-ui";
 
     export let info;
     export let stats;
@@ -10,24 +11,43 @@
     }
 </script>
 <li>
-    <button on:click={btnClick} class="flex items-start p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-slate-700 w-full text-ellipsis">
-        {#if info.isdm}
-            <div class="relative">
-                <ProfilePicture user="{info.dmpartner}"/>
-                {#if !info.dmpartner.isonline}
-                    <span class="bottom-0 left-7 absolute bg-gray-500 dark:bg-gray-600 w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full"></span>
-                {:else}
-                    <span class="bottom-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+    <ContextMenu.Root>
+        <ContextMenu.Trigger class="">
+            <button on:click={btnClick}
+                    class="flex items-start p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-slate-700 w-full text-ellipsis">
+                {#if info.isdm}
+                    <div class="relative">
+                        <ProfilePicture user="{info.dmpartner}"/>
+                        {#if !info.dmpartner.isonline}
+                            <span class="bottom-0 left-7 absolute bg-gray-500 dark:bg-gray-600 w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        {:else}
+                            <span class="bottom-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        {/if}
+                    </div>
                 {/if}
-            </div>
-        {/if}
-        <span class="text-sm text-left ms-3 flex-1 whitespace-nowrap text-truncate">{info.title}</span>
-        { #if stats.unread > 0 }
-            {#if stats.unread_mentions > 0}
-                <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-xs font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">{stats.unread_mentions}</span>
-            {:else}
-                <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-600 dark:text-gray-300">{stats.unread}</span>
-            {/if}
-        {/if}
-    </button>
+                <span class="text-sm text-left ms-3 flex-1 whitespace-nowrap text-truncate">{info.title}</span>
+                { #if stats.unread > 0 }
+                    {#if stats.unread_mentions > 0}
+                        <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-xs font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">{stats.unread_mentions}</span>
+                    {:else}
+                        <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-600 dark:text-gray-300">{stats.unread}</span>
+                    {/if}
+                {/if}
+            </button>
+        </ContextMenu.Trigger>
+        <ContextMenu.Content class="z-50 w-full max-w-[229px] rounded-xl border border-muted bg-background px-1 py-1.5 shadow-popover outline-none">
+            <ContextMenu.Item
+                    class="flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+                <div class="flex items-center">
+                    Pin
+                </div>
+            </ContextMenu.Item>
+            <ContextMenu.Item
+                    class="flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+                <div class="flex items-center">
+                    Hide
+                </div>
+            </ContextMenu.Item>
+        </ContextMenu.Content>
+    </ContextMenu.Root>
 </li>
