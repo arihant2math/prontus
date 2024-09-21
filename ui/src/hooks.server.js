9f1b1b11e2d1655d88933141fc9e74b5d54c59a4
Ashwin Naren <arihant2math@gmail.com>
2024-09-21 12:05:54 -0700

    feat: enabled sentry

--- ui/src/hooks.server.js
+++ ui/src/hooks.server.js
@@ -1,14 +1,14 @@
-// import {sequence} from "@sveltejs/kit/hooks";
-// import {handleErrorWithSentry, sentryHandle} from "@sentry/sveltekit";
-// import * as Sentry from '@sentry/sveltekit';
-//
-// Sentry.init({
-//     dsn: 'https://405651d1f519346b182acb1626e78183@o4507958552297472.ingest.us.sentry.io/4507959128358912',
-//     tracesSampleRate: 1.0,
-// });
-//
-// // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-// export const handle = sequence(sentryHandle());
-//
-// // If you have a custom error handler, pass it to `handleErrorWithSentry`
-// export const handleError = handleErrorWithSentry();
+import {sequence} from "@sveltejs/kit/hooks";
+import {handleErrorWithSentry, sentryHandle} from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+    dsn: 'https://405651d1f519346b182acb1626e78183@o4507958552297472.ingest.us.sentry.io/4507959128358912',
+    tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
