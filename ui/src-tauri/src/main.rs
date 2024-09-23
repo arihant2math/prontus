48394325f931f9482e0be13f0655cbbf348fd474
Ashwin Naren <arihant2math@gmail.com>
2024-09-23 10:08:02 -0700

    chore: added sentry for rust

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    let _guard = sentry::init(("https://11ea16af2a9b5fb2bc56d6283ea0f129@o4507958552297472.ingest.us.sentry.io/4508003269410816", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+    }));
     ui_lib::run()
 }
