7176614409ffc1a0529f8e3126c61570ec55523f
Ashwin Naren <arihant2math@gmail.com>
2024-10-02 20:02:21 -0700

    chore: user category

--- crates/client/src/models/category.rs
+++ crates/client/src/models/category.rs
@@ -1,8 +1,19 @@
 use serde::{Deserialize, Serialize};
 
+struct UserCategory {
+    pub id: i64,
+    pub user_id: i64,
+    pub category_id: i64,
+    pub alias: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Category {
     pub id: u64,
     pub title: String,
     pub sort_order: Option<u32>,
+    #[serde(rename = "usercategory")]
+    pub user_category: UserCategory
 }
