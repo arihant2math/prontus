0f0e380009d6afc27aba6c897fa2e5eec5d891e7
Ashwin Naren <arihant2math@gmail.com>
2024-10-03 17:38:33 -0700

    chore: formatting

--- ui/src/routes/auth/AuthEmail.svelte
+++ ui/src/routes/auth/AuthEmail.svelte
@@ -37,7 +37,7 @@
                                 <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" disabled>
                             </div>
                             <div class="ml-3 text-sm">
-<!--                                TODO: Implement this -->
+                                <!-- TODO: Implement this -->
                                 <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                             </div>
                         </div>
