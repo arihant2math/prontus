b198770d8d47bb7b53191516a01602e99125b4ac
Ashwin Naren <arihant2math@gmail.com>
2024-10-05 00:12:19 -0700

    feat: added user permissions in channel to api

--- crates/client/src/models/bubble.rs
+++ crates/client/src/models/bubble.rs
@@ -20,25 +20,39 @@ pub struct Bubble {
     pub title: String,
     #[serde(rename = "isdm")]
     pub is_dm: bool,
+    pub voice_only: bool,
     #[serde(rename = "deleteanymessage")]
     pub delete_any_message: String,
     #[serde(rename = "changetitle")]
     pub change_title: String,
+    #[serde(rename = "grantchangetitle")]
+    pub grant_change_title: bool,
     #[serde(rename = "changecategory")]
     pub change_category: String,
+    #[serde(rename = "grantchangecategory")]
+    pub grant_change_category: bool,
+    #[serde(rename = "addmember")]
+    pub add_member: String,
+    #[serde(rename = "grantaddmember")]
+    pub grant_add_member: bool,
     #[serde(rename = "removemember")]
     pub remove_member: String,
+    #[serde(rename = "grantremovemember")]
+    pub grant_remove_member: bool,
     #[serde(rename = "leavegroup")]
     pub leave_group: String,
+    #[serde(rename = "grantleavegroup")]
+    pub grant_leave_group: bool,
     #[serde(rename = "deletegroup")]
     pub delete_group: String,
+    #[serde(rename = "grantdeletegroup")]
+    pub grant_delete_group: Option<bool>,
     #[serde(rename = "setrole")]
     pub set_role: String,
     pub create_announcement: String,
     pub assign_task: String,
     pub create_message: String,
     pub grant_create_message: bool,
-    pub voice_only: bool,
     #[serde(rename = "issupergroup")]
     pub is_supergroup: Option<bool>,
     pub archived: u8,
