a83948aea1c7f4f4a651bb5e5c6c3b9be0c15654
Ashwin Naren <arihant2math@gmail.com>
2024-10-06 00:12:37 -0700

    feat: new group/dm buttons

--- ui/src/routes/user/CurrentUserCard.svelte
+++ ui/src/routes/user/CurrentUserCard.svelte
@@ -1,6 +1,7 @@
 <script>
     import ProfilePicture from "./ProfilePicture.svelte";
-    import { DropdownMenu } from "bits-ui";
+    import {Dialog, DropdownMenu, Separator} from "bits-ui";
+    import { fade } from "svelte/transition";
 
     export let user = {
         id: 0,
@@ -16,9 +17,9 @@
     export let showSettings;
 </script>
 
-<div class=" flex flex-row px-4 w-full border-b border-gray-500 z-40">
+<div class="flex flex-row px-4 w-full border-b border-gray-500 z-40">
     <DropdownMenu.Root>
-        <DropdownMenu.Trigger class="flex flex-row space-x-4 p-2 rounded-lg hover:bg-gray-300 dark:hover:bg-slate-700">
+        <DropdownMenu.Trigger class="flex-auto flex flex-row space-x-4 p-2 rounded-lg hover:bg-gray-300 dark:hover:bg-slate-700">
             <div class="align-middle h-full">
                 <ProfilePicture user={user}/>
             </div>
@@ -30,7 +31,7 @@
             </div>
         </DropdownMenu.Trigger>
         <DropdownMenu.Content
-                class="w-full max-w-[229px] rounded-lg border border-muted bg-gray-100 dark:bg-slate-900 px-1 py-1.5 shadow-md"
+                class="w-full max-w-[229px] rounded-lg bg-gray-100 dark:bg-slate-900 px-1 py-1.5 shadow-md"
                 sideOffset={8}
         >
             <DropdownMenu.Item
@@ -42,10 +43,35 @@
             </DropdownMenu.Item>
         </DropdownMenu.Content>
     </DropdownMenu.Root>
-    <button class="ml-auto px-2 rounded-lg hover:bg-gray-300 dark:hover:bg-slate-700" on:click={showSettings}>
+    <button class="px-2 rounded-lg hover:bg-gray-300 dark:hover:bg-slate-700" on:click={showSettings}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
     </button>
+    <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+            <button class="px-2 rounded-lg hover:bg-gray-300 dark:hover:bg-slate-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+            </button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content
+                class="w-full max-w-[229px] rounded-lg bg-gray-100 dark:bg-slate-900 px-1 py-1.5 shadow-md"
+                sideOffset={8}>
+            <DropdownMenu.Item
+                    class="flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+                <button class="flex items-center">
+                    New Group
+                </button>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item
+                    class="flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+                <button class="flex items-center">
+                    New DM
+                </button>
+            </DropdownMenu.Item>
+        </DropdownMenu.Content>
+    </DropdownMenu.Root>
 </div>
