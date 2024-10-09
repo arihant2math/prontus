f1fd17c06d13e43d24d84e04e24d64c5891b578f
Ashwin Naren <arihant2math@gmail.com>
2024-10-08 19:17:47 -0700

    chore: ui macros crate

---
+++ crates/ui_macros/src/lib.rs
@@ -0,0 +1,26 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use syn::parse::Parse;
+
+
+#[proc_macro]
+pub fn unlock(_: TokenStream) -> TokenStream {
+    let expanded = quote::quote! {
+        let state = state.inner().inner();
+        let state = state.read().await;
+        let state = state.try_inner()?;
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro]
+pub fn unlock_mut(_: TokenStream) -> TokenStream {
+    let expanded = quote::quote! {
+        let state = state.inner().inner();
+        let mut state = state.write().await;
+        let state = state.try_inner_mut()?;
+    };
+
+    TokenStream::from(expanded)
+}
