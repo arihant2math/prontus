738c5536ae9cb5e7aeda71a1849eea4708fd6ca1
Ashwin Naren <arihant2math@gmail.com>
2024-10-15 08:46:43 -0700

    chore: minor formatting in extract.py

--- extract.py
+++ extract.py
@@ -2,11 +2,14 @@
 import zipfile
 import os
 
+in_dir = 'dist/'
+out_dir = 'extracted/'
+
 # Gets all the .zip files in the dist/ directory
-zip_files = [f for f in os.listdir('dist/') if f.endswith('.zip')]
+zip_files = [f for f in os.listdir(in_dir) if f.endswith('.zip')]
 
 # Extracts all the .zip files in the dist/ directory
 for zip_file in zip_files:
-    with zipfile.ZipFile('dist/' + zip_file, 'r') as zip_ref:
-        zip_ref.extractall('extracted/')
-        print('Extracted ' + zip_file + ' to extracted/')
+    with zipfile.ZipFile(in_dir + zip_file) as zip_ref:
+        zip_ref.extractall(out_dir)
+        print(f'Extracted {zip_file} to {out_dir}')
