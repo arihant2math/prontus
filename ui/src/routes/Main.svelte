d2721eeb0e787c747fa0b4a9b76df92d428dabeb
Ashwin Naren <arihant2math@gmail.com>
2024-10-27 20:00:37 -0700

    fix: aria label

--- ui/src/routes/Main.svelte
+++ ui/src/routes/Main.svelte
@@ -172,7 +172,7 @@
             {#if showThread}
                 <div class="w-max h-full overflow-x-hidden overflow-y-hidden border border-gray-500">
                     <button class="fixed top-16 right-4 bg-white dark:bg-slate-800 hover:bg-gray-100 dark:hover:bg-slate-700 p-1 rounded-lg"
-                            onclick={() => {showThread = false}}>
+                            onclick={() => {showThread = false}} aria-label="Close Thread">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
