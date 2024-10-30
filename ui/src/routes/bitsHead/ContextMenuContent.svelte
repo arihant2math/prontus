40a0b4c091ac7b5a8c70a8483443f0b7fb4be711
Ashwin Naren <arihant2math@gmail.com>
2024-10-29 18:12:38 -0700

    chore: headed ContextMenuContent

---
+++ ui/src/routes/bitsHead/ContextMenuContent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {ContextMenu} from "bits-ui";
+
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let {children}: Props = $props();
+</script>
+<ContextMenu.Content
+        class="z-50 w-full max-w-[229px] rounded-xl border border-muted bg-white dark:bg-slate-800 px-1 py-1.5 shadow-popover outline-none">
+    {@render children?.()}
+</ContextMenu.Content>
