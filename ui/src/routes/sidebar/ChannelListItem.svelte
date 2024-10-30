99ae30479693bfacc3b18e5b0b83cccd268a4de8
Ashwin Naren <arihant2math@gmail.com>
2024-10-29 18:13:50 -0700

    feat: switched back to non-native right click menu with more features

--- ui/src/routes/sidebar/ChannelListItem.svelte
+++ ui/src/routes/sidebar/ChannelListItem.svelte
@@ -1,8 +1,8 @@
 <script>
     import ProfilePicture from "../user/ProfilePicture.svelte";
-    import {readChannel, setChannelMute, setChannelPin} from "$lib/api.ts";
-    import {MenuItem} from "@tauri-apps/api/menu/menuItem";
-    import {Menu} from "@tauri-apps/api/menu/menu";
+    import {readChannel, setChannelAlias, setChannelMute, setChannelPin} from "$lib/api.ts";
+    import { ContextMenu } from "bits-ui";
+    import ContextMenuContent from "../bitsHead/ContextMenuContent.svelte";
 
     /** @type {{info: any, stats: any, membership: any, buttonClick: any, active?: boolean}} */
     let {
@@ -19,106 +19,76 @@
     let mentionString = $derived(stats.unread_mentions > 99 ? "99+" : stats.unread_mentions);
     let fontWeight = $derived(stats.unread > 0 ? "font-bold" : "font-medium");
     let bgColor = $derived(active ? "bg-blue-600 dark:bg-blue-500 hover:bg-blue-700 dark:hover:bg-blue-600 text-white" : "hover:bg-gray-100 dark:hover:bg-slate-700");
+    let contextMenuOpen = $state(false);
 
     function btnClick() {
         buttonClick(info.id);
-    }
-
-    async function showContextMenu(e) {
-        e.preventDefault();
-
-        let menuItemsPromise = [];
-
-        menuItemsPromise.push(MenuItem.new({
-            text: 'Open',
-            action: () => {
-                buttonClick(info.id);
-            },
-        }));
-
-        if (stats.unread > 0) {
-            menuItemsPromise.push(MenuItem.new({
-                text: 'Mark as Read',
-                action: () => {
-                    readChannel(info.id);
-                },
-            }));
-        }
-        if (membership.is_pinned) {
-            menuItemsPromise.push(MenuItem.new({
-                text: 'Unpin',
-                action: () => {
-                    setChannelPin(info.id, false);
-                },
-            }));
-        } else {
-            menuItemsPromise.push(MenuItem.new({
-                text: 'Pin',
-                action: () => {
-                    setChannelPin(info.id, true);
-                },
-            }));
-        }
-
-        if (membership.mute) {
-
-        } else {
-            menuItemsPromise.push(MenuItem.new({
-                text: 'Unmute',
-                action: () => {
-                    setChannelMute(false)
-                },
-            }));
-        }
-
-        menuItemsPromise.push(MenuItem.new({
-            text: 'Hide',
-            action: () => {
-                // TODO
-            }
-        }))
-        // PredefinedMenuItem.new({ item: 'Separator' }),
-
-        let menuItems = await Promise.all(menuItemsPromise);
-
-        const menu = await Menu.new({
-            items: menuItems,
-        });
-
-        await menu.popup();
     }
 
     async function addListener() {
         while (listItem === null) {
             await new Promise(r => setTimeout(r, 50));
         }
-        listItem.addEventListener("contextmenu", (event) => {
-            showContextMenu(event)
+        window.addEventListener("auxclick", (e) => {
+            if (!listItem.contains(e.target)) {
+                contextMenuOpen = false;
+            }
         });
+        // TODO: listener
     }
 
     addListener();
 </script>
-<li class="select-none" bind:this={listItem} onload={addListener}>
-    <button onclick={() => {btnClick()}}
-            class="flex-0 flex items-start p-2 {textColor} {bgColor} transition duration-75 rounded-lg pl-4 group w-full text-ellipsis">
-        {#if info.isdm}
-            <div class="relative">
-                <ProfilePicture user={info.dmpartner}/>
-                {#if !info.dmpartner.isonline}
-                    <span class="bottom-0 left-7 absolute bg-gray-500 dark:bg-gray-600 w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full"></span>
-                {:else}
-                    <span class="bottom-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+<ContextMenu.Root bind:open={contextMenuOpen}>
+    <ContextMenu.Trigger>
+        <li class="select-none" bind:this={listItem} onload={addListener}>
+            <button onclick={() => {btnClick()}}
+                    class="flex-0 flex items-start p-2 {textColor} {bgColor} transition duration-75 rounded-lg pl-4 group w-full text-ellipsis">
+                {#if info.isdm}
+                    <div class="relative">
+                        <ProfilePicture user={info.dmpartner}/>
+                        {#if !info.dmpartner.isonline}
+                            <span class="bottom-0 left-7 absolute bg-gray-500 dark:bg-gray-600 w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        {:else}
+                            <span class="bottom-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        {/if}
+                    </div>
                 {/if}
-            </div>
+                <span class="flex-1 text-sm text-left rtl:text-right ms-3 whitespace-nowrap text-truncate truncate {fontWeight}">{title}</span>
+                {#if stats.unread > 0 && !membership.mute}
+                    {#if stats.unread_mentions > 0}
+                        <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-white bg-red-600 rounded-full dark:text-white w-fit">{mentionString}</span>
+                    {:else}
+                        <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-600 dark:text-gray-300 w-fit">{unreadString}</span>
+                    {/if}
+                {/if}
+            </button>
+        </li>
+    </ContextMenu.Trigger>
+    <ContextMenuContent>
+        <ContextMenu.Item onclick={() => {btnClick()}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            Open
+        </ContextMenu.Item>
+        <ContextMenu.Item onclick={() => {readChannel(info.id)}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            Mark as read
+        </ContextMenu.Item>
+        <ContextMenu.Item onclick={() => {}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            Set Alias
+        </ContextMenu.Item>
+        {#if membership.alias !== null}
+            <ContextMenu.Item onclick={() => {setChannelAlias(info.id, null)}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+                Remove alias
+            </ContextMenu.Item>
         {/if}
-        <span class="flex-1 text-sm text-left rtl:text-right ms-3 whitespace-nowrap text-truncate truncate {fontWeight}">{title}</span>
-        {#if stats.unread > 0 && !membership.mute}
-            {#if stats.unread_mentions > 0}
-                <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-white bg-red-600 rounded-full dark:text-white w-fit">{mentionString}</span>
-            {:else}
-                <span class="inline-flex items-center justify-center px-2 ms-3 text-xs font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-600 dark:text-gray-300 w-fit">{unreadString}</span>
-            {/if}
-        {/if}
-    </button>
-</li>
+        <ContextMenu.Item onclick={() => {setChannelMute(info.id, !membership.mute)}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            {membership.mute ? "Unmute" : "Mute"}
+        </ContextMenu.Item>
+        <ContextMenu.Item onclick={() => {setChannelPin(info.id, !membership.pin)}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            {membership.pin ? "Unpin" : "Pin"}
+        </ContextMenu.Item>
+        <!-- TODO: -->
+        <ContextMenu.Item onclick={() => {}} class="hover:bg-gray-100 dark:hover:bg-slate-700 flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-none !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            Hide
+        </ContextMenu.Item>
+    </ContextMenuContent>
+</ContextMenu.Root>
