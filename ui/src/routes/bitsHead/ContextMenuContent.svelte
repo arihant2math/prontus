4203fe85670c95542c6ed43a10a89a85dd977963
Ashwin Naren <arihant2math@gmail.com>
2024-11-11 21:45:33 -0800

    fix: border of right click menu

--- ui/src/routes/bitsHead/ContextMenuContent.svelte
+++ ui/src/routes/bitsHead/ContextMenuContent.svelte
@@ -8,6 +8,6 @@
     let {children}: Props = $props();
 </script>
 <ContextMenu.Content
-        class="z-50 w-full max-w-[229px] rounded-xl border border-muted bg-white dark:bg-slate-800 px-1 py-1.5 shadow-popover outline-none">
+        class="z-50 w-full max-w-[229px] rounded-xl bg-white dark:bg-slate-800 px-1 py-1.5 shadow-lg">
     {@render children?.()}
 </ContextMenu.Content>
