9b22a3d69dc6bfefbe9d25c4f8ec4be57f8994bc
Ashwin Naren <arihant2math@gmail.com>
2024-11-19 22:51:35 -0800

    feat: initialize all extensions

--- ui/src-tauri/src/task/extension.rs
+++ ui/src-tauri/src/task/extension.rs
@@ -1,13 +1,19 @@
-use tauri::AppHandle;
 use thiserror::Error;
-use settings::SettingsError;
-use ui_lib::AppState;
+use extension::ExtensionManager;
 
 #[derive(Debug, Error)]
 pub enum ExtensionThreadError {
+    #[error("Extension Error: {0}")]
+    ExtensionError(#[from] extension::LoadExtensionsError)
 }
 
 pub async fn run() -> Result<(), ExtensionThreadError> {
+    let extensions_dir = settings::prontus_dir().join("extensions");
+    let extension_manager = {
+        let mut extension_manager = ExtensionManager::default();
+        extension_manager.load_extensions(extensions_dir).await?;
+        extension_manager
+    };
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
