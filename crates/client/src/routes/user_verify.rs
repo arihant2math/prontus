8f050f98b6a0baaa9630a77dbb1e928d551443f3
Ashwin Naren <arihant2math@gmail.com>
2024-12-28 20:09:53 -0800

    chore: reduced todos

--- crates/client/src/routes/user_verify.rs
+++ crates/client/src/routes/user_verify.rs
@@ -1,37 +1,37 @@
-use crate::APIResult;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use std::{error, fs};
+use std::path::PathBuf;
+use std::sync::Arc;
+use log::warn;
+use thiserror::Error;
+use crate::info::ExtensionInfo;
+pub use crate::wasm_host::WasmExtension;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum UserVerifyRequest {
-    // TODO: Email type
-    #[serde(rename = "email")]
-    Email(String),
-    // example: +14086692442
-    // Which is +[country code][phone number]
-    #[serde(rename = "phone")]
-    Phone(String), // TODO: Phone type
+mod wasm_host;
+pub mod info;
+
+pub const EXTENSION_FILE_NAME: &str = "extension.wasm";
+pub const MANIFEST_FILE_NAME: &str = "manifest.toml";
+
+#[derive(Debug, Error)]
+pub enum LoadExtensionsError {
+    #[error("IO Error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("Extension Error: {0}")]
+    ExtensionError(#[from] wasm_host::WasmExtensionError),
+    #[error("Extension Info Error: {0}")]
+    ExtensionInfoError(#[from] info::ExtensionInfoCreationError),
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct UserVerifyResponse {
-    pub ok: bool,
-    pub length: u16,
+#[derive(Default)]
+pub struct ExtensionManager {
+    extensions: Vec<WasmExtension>,
 }
 
-pub type UserVerifyResult = APIResult<UserVerifyResponse>;
-
-pub async fn post(request: UserVerifyRequest) -> Result<UserVerifyResult, reqwest::Error> {
-    let client = reqwest::Client::new();
-    let resp = match request {
-        UserVerifyRequest::Email(email) => client
-            .post("https://accounts.pronto.io/api/v1/user.verify")
-            .json(&json!({"email": email})),
-        UserVerifyRequest::Phone(phone) => client
-            .post("https://accounts.pronto.io/api/v1/user.verify")
-            .json(&json!({"phone": phone})),
-    };
-    let resp = resp.send().await?;
-    let resp = resp.json().await?;
-    Ok(resp)
-}
+impl ExtensionManager {
+    pub async fn load_extensions(&mut self, extensions_parent_dir: PathBuf) -> Result<(), LoadExtensionsError> {
+        // Every extension is a directory in the extensions_parent_dir
+        if !extensions_parent_dir.exists() {
+            fs::create_dir(&extensions_parent_dir)?;
+        }
+        for entry in fs::read_dir(&extensions_parent_dir)? {
+        
