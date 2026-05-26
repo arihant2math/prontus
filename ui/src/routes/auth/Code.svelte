d9d40a1f2becae02f383ea175509902a6d184df1
Ashwin Naren <arihant2math@gmail.com>
2026-05-25 19:41:55 -0700

    fix code entry

--- ui/src/routes/auth/Code.svelte
+++ ui/src/routes/auth/Code.svelte
@@ -1,35 +1,11 @@
 <script>
     import { PinInput, Toggle } from "bits-ui";
 
+    // TODO: Use bits-ui
     /** @type {{value?: any}} */
     let {value = $bindable([])} = $props();
     // TODO: onchange prop
 
 </script>
 
-<PinInput.Root
-        bind:value
-        class="flex items-center gap-2"
-        type="text"
-        placeholder="0"
->
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-            class="flex h-input w-10 select-none rounded-input border border-foreground bg-grey-100 dark:bg-slate-800 px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.HiddenInput />
-</PinInput.Root>
+<input bind:value>
